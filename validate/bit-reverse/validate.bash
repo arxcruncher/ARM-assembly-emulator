8cccb894ad0f154366a147c30a078ed2703c8e36
arxcruncher <k.de.vogeleer@gmail.com>
2013-10-23 10:01:26 +0200

    Updated validation scripts

--- validate/bit-reverse/validate.bash
+++ validate/bit-reverse/validate.bash
@@ -1,27 +1,22 @@
 #!/bin/bash
 
-echo -e "\nWarning: Emulator.cc must be compiled for auto mode !!"
+$PROG="../../emulator"
 
-for exp in {1..15}
+for algo in {"gold","bloodworth","nbrv","rodriguez","krukar"}
 do
-	echo -e "\nInput Size 2^$exp ~~~~~~~~~~~~~~~~~~~~~~"
-	
-	for algo in {"gold","bloodworth","nbrv","rodriguez","krukar"}
+	for optim in {0,2}
 	do
-		for optim in {0,2}
-		do
-			echo -n "Validating ./emulator snippets/mem-$exp.txt snippets/$algo-$optim.s ... "
-			./emulator snippets/mem-$exp.txt snippets/$algo-$optim.s >& dump.sim
-			cat dump.sim | grep mm | tail -n $(echo "(2^$exp)*2" | bc) | awk '{print $5}' | tr -d '()' > val.mem
-			diffs=$(diff snippets/correct/$exp.ref val.mem | wc -l)
-			if [ $diffs -eq 0 ]
-			then
-				echo -e "\e[00;32mPASSED\e[00m"
-			else
-				echo -e "\e[00;31mFAILED\e[00m"
-				#diff ref.mem val.mem
-			fi
-		done
+		echo -n "Validating $PROG mem-$exp.txt $algo-$optim.s ... "
+		$PROG mem-10.txt $algo-$optim.s >& dump.sim
+		cat dump.sim | grep mm | tail -n $(echo "(2^10)*2" | bc) | awk '{print $5}' | tr -d '()' > val.mem
+		diffs=$(diff correct/10.ref val.mem | wc -l)
+		if [ $diffs -eq 0 ]
+		then
+			echo -e "\e[00;32mPASSED\e[00m"
+		else
+			echo -e "\e[00;31mFAILED\e[00m"
+			#diff ref.mem val.mem
+		fi
 	done
 done
 
