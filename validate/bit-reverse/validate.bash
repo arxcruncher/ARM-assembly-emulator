2a149889d352b738809d7a7a8c022fb8881b503b
arxcruncher <k.de.vogeleer@gmail.com>
2013-10-24 18:16:22 +0200

    Patched validation script, now it is working

--- validate/bit-reverse/validate.bash
+++ validate/bit-reverse/validate.bash
@@ -1,13 +1,13 @@
 #!/bin/bash
 
-$PROG="../../emulator"
+PROG="../../emulator"
 
 for algo in {"gold","bloodworth","nbrv","rodriguez","krukar"}
 do
 	for optim in {0,2}
 	do
 		echo -n "Validating $PROG mem-$exp.txt $algo-$optim.s ... "
-		$PROG mem-10.txt $algo-$optim.s >& dump.sim
+		$PROG -m mem-10.txt -p $algo-$optim.s >& dump.sim
 		cat dump.sim | grep mm | tail -n $(echo "(2^10)*2" | bc) | awk '{print $5}' | tr -d '()' > val.mem
 		diffs=$(diff correct/10.ref val.mem | wc -l)
 		if [ $diffs -eq 0 ]
